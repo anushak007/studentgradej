39ddd132bbe98d9404653765d6de144616eb4e2d
anushak007 <anushakulkarni007@gmail.com>
2025-11-11 09:00:01 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,6 +1,6 @@
 import sys
 
-if len(sys.argv) == 6:  # script name + 5 arguments
+if len(sys.argv) == 6:  # script name + 5 marks
     m1 = int(sys.argv[1])
     m2 = int(sys.argv[2])
     m3 = int(sys.argv[3])
@@ -17,5 +17,20 @@ else:
 total = m1 + m2 + m3 + m4 + m5
 avg = total / 5
 
+# Determine grade based on average
+if avg >= 90:
+    grade = "A+"
+elif avg >= 80:
+    grade = "A"
+elif avg >= 70:
+    grade = "B"
+elif avg >= 60:
+    grade = "C"
+elif avg >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
 print("The total marks is:", total)
 print("The average is:", avg)
+print("The grade is:", grade)
